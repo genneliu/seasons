9fa91c45c45b573e678c2c86d4e5a4ac6c5cd028
Genne Liu <genneliu@gmail.com>
2021-10-20 10:02:03 -0700

    constructor method

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 
 /*functional component
 const App = () => {
-    
     );
     return <div>Latitude: </div>
 };
 */
 
-//class based component
+//class based component 
 //extends allows us to pull built in functionality from class React.component
 //subclassing react.component
 
 class App extends React.Component {
+    //not required by react, helpful to initialize state
+    constructor(props){
+        //React.component has own constructor, so super 
+        super(props)
+        //this object is for initializing state
+        //null becaue we dont now what the lat is right now
+        this.state = { lat: null };
+    }
+
+    //react requires to define render
     render() {
         window.navigator.geolocation.getCurrentPosition(
     //first callback when location successful
