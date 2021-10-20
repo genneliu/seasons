36c309e0c28454c8fa5e211f63152756db92a236
Genne Liu <genneliu@gmail.com>
2021-10-20 09:31:34 -0700

    Seasons app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*functional component
+const App = () => {
+    
+    );
+    return <div>Latitude: </div>
+};
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//class based component
+//extends allows us to pull built in functionality from class React.component
+//subclassing react.component
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+    //first callback when location successful
+        position => console.log(position),
+    //failure callbac
+        err => console.log(err)
+        );
+
+    return <div> Latitude: </div>;
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
+
