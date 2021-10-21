c21638277e528a2c4cc0753e995a059af41e02e5
Genne Liu <genneliu@gmail.com>
2021-10-21 10:12:55 -0700

    css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,18 @@
+import './SeasonDisplay.css';
+
 import React from 'react';
+
+
+const seasonConfig = {
+    summer: {
+        text: 'lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'burr, its chilly!',
+        iconName: 'snowflake'
+    }
+};
 
 //extract logic out of functional componenets as possible
 const getSeason = (lat, month) => {
@@ -10,20 +24,25 @@ const getSeason = (lat, month) => {
 };
 
 //determine if it is summer or winter. also the month
+
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season == 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'
-    //display text and icons
-    const icon = season == 'winter' ? 'snowflake' : 'sun';
+    const { text, iconName } = seasonConfig[season]; // {text, iconName}
+    //refactor ternary expressions, create configuration object at top
+    // const text = season == 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'
+    // //display text and icons
+    // const icon = season == 'winter' ? 'snowflake' : 'sun';
+
+
 
     //can use js expression or variable inside {}
     return (
-        <div>
-            <i className={`${icon} icon`}/> 
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}/> 
             <h1> {text} </h1>
-            <i className={`${icon} icon`}/> 
+            <i className={`icon-right massive ${iconName} icon`}/> 
         </div>
-    )
-}
+    );
+};
 
 export default SeasonDisplay;
